e229e6dbb6f03ca9bd6908536131c55b187d95bc
José Pablo Parajeles <josepabloparaj@gmail.com>
2015-08-31 18:25:12 -0600

    To String

--- settings.py
+++ settings.py
@@ -162,8 +162,8 @@ resultados = {
         },
         "Carril": {"type": "number"},
         "Hit": {"type": "number"},
-        "TiempoRegistro": {"type": "number"},
-        "TiempoRealizado": {"type": "number"},
+        "TiempoRegistro": {"type": "string"},
+        "TiempoRealizado": {"type": "string"},
         "Puntos": {"type": "number"}
     }
 }
@@ -188,90 +188,6 @@ log = {
     "resource_methods":["POST"],
     "item_methods":[]
 }
-
-"""
-people = {
-    # 'title' tag used in item links.
-    'item_title': 'person',
-
-    # by default the standard item entry point is defined as
-    # '/people/<ObjectId>/'. We leave it untouched, and we also enable an
-    # additional read-only entry point. This way consumers can also perform GET
-    # requests at '/people/<lastname>/'.
-    'additional_lookup': {
-        'url': 'regex("[\w]+")',
-        'field': 'lastname'
-    },
-
-    # Schema definition, based on Cerberus grammar. Check the Cerberus project
-    # (https://github.com/nicolaiarocci/cerberus) for details.
-    'schema': {
-        'firstname': {
-            'type': 'string',
-            'minlength': 1,
-            'maxlength': 10,
-        },
-        'lastname': {
-            'type': 'string',
-            'minlength': 1,
-            'maxlength': 15,
-            'required': True,
-            # talk about hard constraints! For the purpose of the demo
-            # 'lastname' is an API entry-point, so we need it to be unique.
-            'unique': True,
-        },
-        # 'role' is a list, and can only contain values from 'allowed'.
-        'role': {
-            'type': 'list',
-            'allowed': ["author", "contributor", "copy"],
-        },
-        # An embedded 'strongly-typed' dictionary.
-        'location': {
-            'type': 'dict',
-            'schema': {
-                'address': {'type': 'string'},
-                'city': {'type': 'string'}
-            },
-        },
-        'born': {
-            'type': 'datetime',
-        },
-    }
-}
-
-works = {
-    # if 'item_title' is not provided Eve will just strip the final
-    # 's' from resource name, and use it as the item_title.
-    # 'item_title': 'work',
-
-    # We choose to override global cache-control directives for this resource.
-    'cache_control': 'max-age=10,must-revalidate',
-    'cache_expires': 10,
-
-    'schema': {
-        'title': {
-            'type': 'string',
-            'required': True,
-        },
-        'description': {
-            'type': 'string',
-        },
-        'owner': {
-            'type': 'objectid',
-            'required': True,
-            # referential integrity constraint: value must exist in the
-            # 'people' collection. Since we aren't declaring a 'field' key,
-            # will default to `people._id` (or, more precisely, to whatever
-            # ID_FIELD value is).
-            'data_relation': {
-                'resource': 'people',
-                # make the owner embeddable with ?embedded={"owner":1}
-                'embeddable': True
-            },
-        },
-    }
-}
-"""
 
 
 # The DOMAIN dict explains which resources will be available and how they will
